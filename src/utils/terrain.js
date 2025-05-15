d0641a006d52a08e73d7f191b73d6e3bf785c2ef
Lucciano Barberan <luccianobarberan@gmail.com>
2025-05-15 09:02:31 -0700

    added terrain types

---
+++ src/utils/terrain.js
@@ -0,0 +1,23 @@
+export const TERRAIN_TYPES = {
+    desert: {
+      name: 'Desert',
+      color: '#FFD449'
+    },
+    grassland: {
+      name: 'Grassland',
+      color: '#548C2F'
+    },
+    forest: {
+      name: 'Forest',
+      color: '#104911'
+    },
+    mountain: {
+      name: 'Mountain',
+      color: '#656D4A'
+    },
+    peak: {
+      name: 'Mountain Peak',
+      color: '#DAD7CD'
+    }
+  };
+  
